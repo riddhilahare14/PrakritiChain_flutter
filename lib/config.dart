5c249e90179cc8fe0c5055f0332c4d153d69679d
riddhilahare14 <lahare.riddhi14@gmail.com>
2025-09-17 20:53:07 +0530

    internet permission

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,7 @@
 // lib/config.dart
 class AppConfig {
-  static const String baseUrl = "https://0b7fa9f074e9.ngrok-free.app"; // dev
+  // static const String baseUrl = "https://0b7fa9f074e9.ngrok-free.app"; // dev
+    static const String baseUrl = "https://6b36d07c1ac7.ngrok-free.app"; // dev
+
   // static const String baseUrl = "https://myproductionapi.com"; // prod
 }
