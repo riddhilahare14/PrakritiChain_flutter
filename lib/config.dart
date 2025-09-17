e8b8bc03a88ec218dabac13afdd42cf8979b0d72
riddhilahare14 <lahare.riddhi14@gmail.com>
2025-09-17 23:38:35 +0530

    changed app name

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 // lib/config.dart
 class AppConfig {
   // static const String baseUrl = "https://0b7fa9f074e9.ngrok-free.app"; // dev
-    static const String baseUrl = "https://propitiously-perforable-khalilah.ngrok-free.app"; // dev
+    static const String baseUrl = "https://acrosporous-rikki-superinfinitely.ngrok-free.app"; // dev
 
   // static const String baseUrl = "https://myproductionapi.com"; // prod
 }
